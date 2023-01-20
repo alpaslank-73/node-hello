80a5141e8ed03fb954d6699a8d2f76b5d5c88188
Alpaslan Kaplan <alp@perception.com.tr>
2023-01-20 07:17:27 -0500

    Updated http reponse

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end('<html><body><h1>Hello World from Node.js</h1></body></html>');
+ res.end('<h1>Hello World from Node.js</h1>\n');
 })
 ; server.listen(port, 'localhost');
 server.listen(port, '0.0.0.0');
